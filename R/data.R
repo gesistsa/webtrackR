8aa36d7c25a8c1510d9727c0dfe966744d89e835
schochastics <david@schochastics.net>
2022-09-09 09:10:49 +0200

    added domain list

--- R/data.R
+++ R/data.R
@@ -1,7 +1,11 @@
 #' Test Data (needs to be removed)
 "test_data"
 
-#' News Domains
+#' News Types
 #'
 #' probably from some Stier et al. paper
 "news_types"
+
+#' Domain list
+#' classification of some domains (probably from some Stier et al. paper)
+"domain_list"
