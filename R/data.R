4a34c3950fb2f2360371c42defec7b33be81952b
schochastics <david@schochastics.net>
2022-09-09 21:25:52 +0200

    forgot to commit :)

--- R/data.R
+++ R/data.R
@@ -9,3 +9,8 @@
 #' Domain list
 #' classification of some domains (probably from some Stier et al. paper)
 "domain_list"
+
+#' Bakshy Top500
+#' Ideological alignment of 500 domains based on facebook
+#' @references Bakshy, Eytan, Solomon Messing, and Lada A. Adamic. "Exposure to ideologically diverse news and opinion on Facebook." Science 348.6239 (2015): 1130-1132.
+"bakshy"
