74be8a70408199506bd2bb2d6a497f0a92a97a4e
schochastics <david@schochastics.net>
2023-03-07 20:22:51 +0100

    fixed doi handling

--- R/data.R
+++ R/data.R
@@ -13,13 +13,13 @@
 #'
 #' Classification of domains into different news types
 #' @references
-#'  Stier, S., Mangold, F., Scharkow, M., & Breuer, J. (2022). Post Post-Broadcast Democracy? News Exposure in the Age of Online Intermediaries. American Political Science Review, 116(2), 768-774. doi:10.1017/S0003055421001222
+#'  Stier, S., Mangold, F., Scharkow, M., & Breuer, J. (2022). Post Post-Broadcast Democracy? News Exposure in the Age of Online Intermediaries. American Political Science Review, 116(2), 768-774.
 "news_types"
 
 #' Domain list
 #' classification of domains into news,portals, search, and social media
 #' @references
-#'  Stier, S., Mangold, F., Scharkow, M., & Breuer, J. (2022). Post Post-Broadcast Democracy? News Exposure in the Age of Online Intermediaries. American Political Science Review, 116(2), 768-774. doi:10.1017/S0003055421001222
+#'  Stier, S., Mangold, F., Scharkow, M., & Breuer, J. (2022). Post Post-Broadcast Democracy? News Exposure in the Age of Online Intermediaries. American Political Science Review, 116(2), 768-774.
 "domain_list"
 
 #' Bakshy Top500
