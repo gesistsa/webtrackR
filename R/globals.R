2ae88adc508abebb4cd64fb9e0e5cf1614501ae9
schochastics <david@schochastics.net>
2023-05-12 16:57:30 +0200

    fixed pr issues

--- R/globals.R
+++ R/globals.R
@@ -1,1 +1,1 @@
-utils::globalVariables(c("duration", "timestamp", "panelist_id","domain","visit","day","type","prev_type","tmp","session"))
+utils::globalVariables(c("duration", "timestamp", "panelist_id","domain","visit","day","type","prev_type","tmp","session","host","suffix","domain_name"))
