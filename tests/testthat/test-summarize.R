1397678d2149fab0560461a3e33e9bbea4095cf1
schochastics <david@schochastics.net>
2023-06-27 20:26:00 +0200

    fixed formatting and error handling

--- tests/testthat/test-summarize.R
+++ tests/testthat/test-summarize.R
@@ -2,8 +2,8 @@ test_that("sum_visits", {
   data("test_data")
   wt <- as.wt_dt(test_data)
   wt <- extract_domain(wt)
-  wt[,google:=ifelse(domain == "google.com", 1, 0)]
-  wt[,search:=ifelse(grepl("search", url), 1, 0)]
+  wt[,google := ifelse(domain == "google.com", 1, 0)]
+  wt[,search := ifelse(grepl("search", url), 1, 0)]
   wt_sum <- sum_visits(wt)
   expect_true("n_visits" %in% names(wt_sum))
   expect_true("panelist_id" %in% names(wt_sum))
@@ -16,8 +16,8 @@ test_that("sum_visits errors", {
   data("test_data")
   wt <- as.wt_dt(test_data)
   wt <- extract_domain(wt)
-  wt[,google:=ifelse(domain == "google.com", 1, 0)]
-  wt[,search:=ifelse(grepl("search", url), 1, 0)]
+  wt[,google := ifelse(domain == "google.com", 1, 0)]
+  wt[,search := ifelse(grepl("search", url), 1, 0)]
   expect_error(sum_visits(wt, timeframe = "something"))
   expect_error(sum_visits(wt, timeframe = "wave"))
 })
