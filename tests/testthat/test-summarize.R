16079a215b538c3f8a1b40e096af9ed1885cf249
BernhardClemm <bernhard.clemm@mail.de>
2023-06-28 12:43:22 +0200

    documentation

--- tests/testthat/test-summarize.R
+++ tests/testthat/test-summarize.R
@@ -7,15 +7,19 @@ test_that("sum_visits", {
   wt_sum <- sum_visits(wt)
   expect_true("n_visits" %in% names(wt_sum))
   expect_true("panelist_id" %in% names(wt_sum))
-  
   expect_true(!"all" %in% names(wt_sum))
-  
+  expect_true(length(unique(wt_sum$panelist_id)) == length(unique(wt$panelist_id)))
+  expect_true(nrow(wt) == sum(wt_sum$n_visits, na.rm = T))
+  wt_sum_date <- sum_visits(wt, timeframe = "date")
+  expect_true("date" %in% names(wt_sum_date))
   wt_sum_week <- sum_visits(wt, timeframe = "week")
   expect_true("week" %in% names(wt_sum_week))
-  
-  # expect_true(sum_visits(wt), is.null(timeframe_var)) # this does not work, I assume because timeframe_var is not carried outside function
-  # also add other cases for timeframe_var
-  # check whether grouping variables in wt_sum
+  wt_sum_month <- sum_visits(wt, timeframe = "month")
+  expect_true("month" %in% names(wt_sum_month))
+  wt_sum_year <- sum_visits(wt, timeframe = "year")
+  expect_true("year" %in% names(wt_sum_year))
+  wt_sum_wave <- sum_visits(wt, timeframe = "wave")
+  expect_true("wave" %in% names(wt_sum_wave))
 })
 
 test_that("sum_visits errors", {
@@ -24,6 +28,43 @@ test_that("sum_visits errors", {
   wt <- extract_domain(wt)
   wt[,google := ifelse(domain == "google.com", 1, 0)]
   wt[,search := ifelse(grepl("search", url), 1, 0)]
-  expect_error(sum_visits(wt, timeframe = "something"))
-  # expect_error(sum_visits(wt, timeframe = "wave"))
+  expect_error(sum_visits(wt, timeframe = "not_a_variable"))
+  wt[["wave"]] <- NULL
+  expect_error(sum_durations(wt, timeframe = "wave"))
+})
+
+test_that("sum_durations", {
+  data("testdt_tracking")
+  wt <- as.wt_dt(testdt_tracking)
+  wt <- extract_domain(wt)
+  wt[,google := ifelse(domain == "google.com", 1, 0)]
+  wt[,search := ifelse(grepl("search", url), 1, 0)]
+  wt_sum <- sum_durations(wt)
+  expect_true("duration_visits" %in% names(wt_sum))
+  expect_true("panelist_id" %in% names(wt_sum))
+  expect_true(!"all" %in% names(wt_sum))
+  expect_true(length(unique(wt_sum$panelist_id)) == length(unique(wt$panelist_id)))
+  expect_true(sum(wt$duration, na.rm = T) == sum(wt_sum$duration_visits, na.rm = T))
+  wt_sum_date <- sum_durations(wt, timeframe = "date")
+  expect_true("date" %in% names(wt_sum_date))
+  wt_sum_week <- sum_durations(wt, timeframe = "week")
+  expect_true("week" %in% names(wt_sum_week))
+  wt_sum_month <- sum_durations(wt, timeframe = "month")
+  expect_true("month" %in% names(wt_sum_month))
+  wt_sum_year <- sum_durations(wt, timeframe = "year")
+  expect_true("year" %in% names(wt_sum_year))
+  wt_sum_wave <- sum_durations(wt, timeframe = "wave")
+  expect_true("wave" %in% names(wt_sum_wave))
+})
+
+test_that("sum_durations errors", {
+  data("testdt_tracking")
+  wt <- as.wt_dt(testdt_tracking)
+  wt <- extract_domain(wt)
+  wt[,google := ifelse(domain == "google.com", 1, 0)]
+  wt[,search := ifelse(grepl("search", url), 1, 0)]
+  expect_error(sum_durations(wt, duration_var = "not_a_variable"))
+  expect_error(sum_durations(wt, timeframe = "not_a_variable"))
+  wt[["wave"]] <- NULL
+  expect_error(sum_durations(wt, timeframe = "wave"))
 })
