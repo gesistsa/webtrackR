bb14f0e32f775c979b894aa807e5ce870292eb8a
BernhardClemm <bernhard.clemm@mail.de>
2023-07-14 11:49:50 +0200

    summarize wide

--- R/summarize.R
+++ R/summarize.R
@@ -1,90 +1,96 @@
 #' Summarize number of visits by person
 #' @description Summarize number of visits by person within a time frame, and optionally by class of visit
 #' @details sum_visits allows you to summarize the number of visits by panelist_id for different time periods (for example, by day).
-#' It further allows to break down the number by any set of "classes" of visits, e.g. the type of the visit's domain.
+#' It further allows to break down the number by any "classes" of visits.
 #' @param wt webtrack data object.
-#' @param timeframe character. indicates for what time frame to aggregate visits. Possible values are "date", "week", "month", "year", "wave" or "all".
-#' If set to "wave", webtrack data object must contain a column call "wave". Defaults to "all".
-#' @param visit_class character vector. Column(s) that contains a classification of visits.
-#' Visits will be grouped by values in this column before being summarized. Defaults to NULL.
-#' @importFrom data.table is.data.table shift .N
-#' @return a data.table with columns "panelist_id", column indicating the time unit unless "all" was specified,
-#' name indicating the class variable if specified, and "n_visits" indicating the number of visits
+#' @param timeframe character. indicates for what time frame to aggregate visits. Possible values are "date", "week", "month", "year", "wave" or NULL.
+#' If set to "wave", webtrack data object must contain a column call "wave". Defaults to NULL, in which case the output contains number of visits for the entire time.
+#' @param visit_class character. Column that contains a classification of visits. For each value in this column,
+#' the output will have a column indicating the number of visits belonging to that value. Defaults to NULL.
+#' @importFrom data.table is.data.table shift .N dcast setnames
+#' @return a data.table with columns "panelist_id", column indicating the time unit (unless 'timeframe' set ot NULL),
+#' "n_visits" indicating the number of visits, and a column for each value of the class (if 'visit_class' not set to NULL).
 #' @examples
 #' \dontrun{
 #' data("testdt_tracking")
 #' wt <- as.wt_dt(testdt_tracking)
-#' # example of visit classification
 #' wt <- extract_domain(wt, drop_na = T)
 #' wt[, google := ifelse(domain == "google.com", 1, 0)]
-#' wt[, search := ifelse(grepl("search", url), 1, 0)]
-#' summary <- sum_visits(wt, timeframe = "month", visit_class = c("google", "search"))
+#' wt_summ <- sum_visits(wt, timeframe = NULL, visit_class = NULL)
+#' wt_summ <- sum_visits(wt, timeframe = "week", visit_class = NULL)
+#' wt_summ <- sum_visits(wt, timeframe = "week", visit_class = "google")
 #' }
 #' @export
-sum_visits <- function(wt, timeframe = "all", visit_class = NULL) {
+sum_visits <- function(wt, timeframe = NULL, visit_class = NULL) {
   stopifnot("input is not a wt_dt object" = is.wt_dt(wt))
-  timeframe <- match.arg(timeframe, c("all", "date", "week", "month", "year", "wave"))
+  match.arg(timeframe, c(NULL, "date", "week", "month", "year", "wave"))
   vars_exist(wt, vars = c("url", "panelist_id", "timestamp"))
-  if (timeframe == "all") {
-    timeframe_var <- NULL
-  } else if (timeframe == "date") {
-    wt[, `:=`(date = format(timestamp, format = "%F"))]
-    timeframe_var <- "date"
-  } else if (timeframe == "week") {
-    wt[, `:=`(week = format(timestamp, format = "%Y week %W"))]
-    timeframe_var <- "week"
-  } else if (timeframe == "month") {
-    wt[, `:=`(month = format(timestamp, format = "%Y month %m"))]
-    timeframe_var <- "month"
-  } else if (timeframe == "year") {
-    wt[, `:=`(year = format(timestamp, format = "%Y"))]
-    timeframe_var <- "year"
-  } else if (timeframe == "wave") {
-    vars_wt <- names(wt)
-    wave <- pmatch("wave", vars_wt)
-    if (is.na(wave)) {
-      stop("couldn't find the column 'wave' in the webtrack data", call. = FALSE)
-    } else {
-      timeframe_var <- "wave"
+  if (!is.null(timeframe)) {
+    if (timeframe == "date") {
+      wt[, `:=`(date = format(timestamp, format = "%F"))]
+    } else if (timeframe == "week") {
+      wt[, `:=`(week = format(timestamp, format = "%Y week %W"))]
+    } else if (timeframe == "month") {
+      wt[, `:=`(month = format(timestamp, format = "%Y month %m"))]
+    } else if (timeframe == "year") {
+      wt[, `:=`(year = format(timestamp, format = "%Y"))]
+    } else if (timeframe == "wave") {
+      stopifnot("couldn't find the column 'wave' in the webtrack data" = "wave" %in% names(wt))
     }
   }
-  grp_vars <- c("panelist_id", timeframe_var, visit_class)
+  grp_vars <- c("panelist_id", timeframe)
   summary <- wt[, list("n_visits" = .N), by = grp_vars]
+  if (!is.null(visit_class)) {
+    wt <- wt[, tmp_class := paste0("n_visits_", visit_class, "_", get(visit_class))]
+    grp_vars <- c("panelist_id", timeframe, "tmp_class")
+    tmp_summary <- wt[, list("tmp_visits" = .N), by = grp_vars]
+    if (is.null(timeframe)) {
+      tmp_summary <- dcast(tmp_summary, panelist_id ~ tmp_class, value.var = c("tmp_visits"), fill = 0)
+    } else {
+      setnames(tmp_summary, timeframe, "tmp_timeframe")
+      tmp_summary <- dcast(tmp_summary, panelist_id + tmp_timeframe ~ tmp_class, value.var = c("tmp_visits"), fill = 0)
+      setnames(tmp_summary, "tmp_timeframe", timeframe)
+    }
+    wt[,tmp_class:=NULL]
+    summary <- summary[tmp_summary, on = c("panelist_id", timeframe)]
+  }
+  if (!is.null(timeframe)) {
+    wt[[timeframe]] <- NULL
+  }
   summary[]
 }
 
 #' Summarize visit duration by person
 #' @description Summarize duration of visits by person within a time frame, and optionally by class of visit
 #' @details sum_durations allows you to sum the duration of visits by panelist_id for different time periods (for example, by day).
-#' It further allows to break down the number by any set of "classes" of visits, e.g. the type of the visit's domain.
+#' It further allows to break down the number by any "classes" of visits.
+#' Note:
+#' - If for a time frame all rows are NA on the duration column, the summarized duration for that time frame will be NA.
+#' - If only some of the rows of a time frame are NA on the duration column, the function will ignore those NA rows.
+#' - If there were no visits to a class (i.e., a value of the 'visit_class' column) for a time frame, the summarized duration for that time frame will be zero; if there were visits, but NA on duration, the summarized duration will be NA.
 #' @param wt webtrack data object.
-#' @param var_duration character. Name of the duration variable if already present. Defaults to NULL,
-#' in which case duration will be approximated with add_duration(wt, cutoff = 300, replace_by = "na", replace_val = NULL)
-#' @param timeframe character. Indicates for what time frame to aggregate visits. Possible values are "date", "week", "month", "year", "wave" or "all".
-#' If set to "wave", webtrack data object must contain a column call "wave". Defaults to "all".
-#' @param visit_class character vector. Column(s) that contains a classification of visits.
-#' Visits will be grouped by values in this column before being summarized. Defaults to NULL.
-#' @importFrom data.table is.data.table shift .N
-#' @return a data.table with columns "panelist_id", column indicating the time unit unless "all" was specified,
-#' name indicating the class variable if specified, and "duration_visits" indicating the duration of visits
-#' (in seconds, or whatever the unit of the variable specified by "var_duration" parameter)
+#' @param timeframe character. indicates for what time frame to aggregate visits. Possible values are "date", "week", "month", "year", "wave" or NULL.
+#' If set to "wave", webtrack data object must contain a column call "wave". Defaults to NULL, in which case the output contains number of visits for the entire time.
+#' @param visit_class character. Column that contains a classification of visits. For each value in this column,
+#' the output will have a column indicating the number of visits belonging to that value. Defaults to NULL.
+#' @importFrom data.table is.data.table shift .N dcast setnames
+#' @return a data.table with columns "panelist_id", column indicating the time unit (unless 'timeframe' set ot NULL),
+#' "duration_visits" indicating the duration of visits (in seconds, or whatever the unit of the variable specified by "var_duration" parameter),
+#' and a column for each value of the class (if 'visit_class' not set to NULL).
 #' @examples
 #' \dontrun{
 #' data("testdt_tracking")
 #' wt <- as.wt_dt(testdt_tracking)
-#' # example of visit classification
-#' wt <- extract_domain(wt)
+#' wt <- extract_domain(wt, drop_na = T)
 #' wt[, google := ifelse(domain == "google.com", 1, 0)]
-#' wt[, search := ifelse(grepl("search", url), 1, 0)]
-#' summary <- sum_durations(wt,
-#'   var_duration = NULL, timeframe = "month",
-#'   visit_class = c("google", "search")
-#' )
+#' wt_summ <- sum_durations(wt)
+#' wt_summ <- sum_durations(wt, var_duration = NULL, timeframe = "week", visit_class = NULL)
+#' wt_summ <- sum_durations(wt, var_duration = NULL, timeframe = "week", visit_class = "google")
 #' }
 #' @export
-sum_durations <- function(wt, var_duration = NULL, timeframe = "all", visit_class = NULL) {
+sum_durations <- function(wt, var_duration = NULL, timeframe = NULL, visit_class = NULL) {
   stopifnot("input is not a wt_dt object" = is.wt_dt(wt))
-  timeframe <- match.arg(timeframe, c("all", "date", "week", "month", "year", "wave"))
+  match.arg(timeframe, c("date", "week", "month", "year", "wave", NULL))
   vars_exist(wt, vars = c("url", "panelist_id", "timestamp"))
   if (is.null(var_duration)) {
     wt <- add_duration(wt)
@@ -92,31 +98,39 @@ sum_durations <- function(wt, var_duration = NULL, timeframe = "all", visit_clas
     vars_exist(wt, vars = c(var_duration))
     colnames(wt)[colnames(wt) == var_duration] <- "duration"
   }
-  if (timeframe == "all") {
-    timeframe_var <- NULL
-  } else if (timeframe == "date") {
-    wt[, `:=`(date = format(timestamp, format = "%F"))]
-    timeframe_var <- "date"
-  } else if (timeframe == "week") {
-    wt[, `:=`(week = format(timestamp, format = "%Y week %W"))]
-    timeframe_var <- "week"
-  } else if (timeframe == "month") {
-    wt[, `:=`(month = format(timestamp, format = "%Y month %m"))]
-    timeframe_var <- "month"
-  } else if (timeframe == "year") {
-    wt[, `:=`(year = format(timestamp, format = "%Y"))]
-    timeframe_var <- "year"
-  } else if (timeframe == "wave") {
-    vars_wt <- names(wt)
-    wave <- pmatch("wave", vars_wt)
-    if (is.na(wave)) {
-      stop("couldn't find the column 'wave' in the webtrack data", call. = FALSE)
-    } else {
-      timeframe_var <- "wave"
+  if (!is.null(timeframe)) {
+    if (timeframe == "date") {
+      wt[, `:=`(date = format(timestamp, format = "%F"))]
+    } else if (timeframe == "week") {
+      wt[, `:=`(week = format(timestamp, format = "%Y week %W"))]
+    } else if (timeframe == "month") {
+      wt[, `:=`(month = format(timestamp, format = "%Y month %m"))]
+    } else if (timeframe == "year") {
+      wt[, `:=`(year = format(timestamp, format = "%Y"))]
+    } else if (timeframe == "wave") {
+      stopifnot("couldn't find the column 'wave' in the webtrack data" = "wave" %in% names(wt))
     }
   }
-  grp_vars <- c("panelist_id", timeframe_var, visit_class)
-  summary <- wt[, list("duration_visits" = sum(duration, na.rm = T)), by = grp_vars]
+
+  grp_vars <- c("panelist_id", timeframe)
+  summary <- wt[, list("duration_visits" = if(all(is.na(duration))) NA_real_ else sum(duration, na.rm = TRUE)), by = grp_vars]
+  if (!is.null(visit_class)) {
+    wt <- wt[, tmp_class := paste0("duration_visits_", visit_class, "_", get(visit_class))]
+    grp_vars <- c("panelist_id", timeframe, "tmp_class")
+    tmp_summary <- wt[, list("tmp_duration" = if(all(is.na(duration))) NA_real_ else sum(duration, na.rm = TRUE)), by = grp_vars]
+    if (is.null(timeframe)) {
+      tmp_summary <- dcast(tmp_summary, panelist_id ~ tmp_class, value.var = c("tmp_duration"), fill = 0)
+    } else {
+      setnames(tmp_summary, timeframe, "tmp_timeframe")
+      tmp_summary <- dcast(tmp_summary, panelist_id + tmp_timeframe ~ tmp_class, value.var = c("tmp_duration"), fill = 0)
+      setnames(tmp_summary, "tmp_timeframe", timeframe)
+    }
+    wt[,tmp_class:=NULL]
+    summary <- summary[tmp_summary, on = c("panelist_id", timeframe)]
+  }
+  if (!is.null(timeframe)) {
+    wt[[timeframe]] <- NULL
+  }
   summary[]
 }
 
--- tests/testthat/test-summarize.R
+++ tests/testthat/test-summarize.R
@@ -3,11 +3,10 @@ test_that("sum_visits", {
   wt <- as.wt_dt(testdt_tracking)
   wt <- extract_domain(wt)
   wt[, google := ifelse(domain == "google.com", 1, 0)]
-  wt[, search := ifelse(grepl("search", url), 1, 0)]
   wt_sum <- sum_visits(wt)
   expect_true("n_visits" %in% names(wt_sum))
   expect_true("panelist_id" %in% names(wt_sum))
-  expect_true(!"all" %in% names(wt_sum))
+  expect_true(length(names(wt_sum)) == 2)
   expect_true(length(unique(wt_sum$panelist_id)) == length(unique(wt$panelist_id)))
   expect_true(nrow(wt) == sum(wt_sum$n_visits, na.rm = T))
   wt_sum_date <- sum_visits(wt, timeframe = "date")
@@ -20,6 +19,10 @@ test_that("sum_visits", {
   expect_true("year" %in% names(wt_sum_year))
   wt_sum_wave <- sum_visits(wt, timeframe = "wave")
   expect_true("wave" %in% names(wt_sum_wave))
+  wt_sum_google <- sum_visits(wt, visit_class = "google")
+  expect_true("n_visits_google_0" %in% names(wt_sum_google))
+  expect_true("n_visits_google_1" %in% names(wt_sum_google))
+  expect_true(sum(wt_sum_google$n_visits_google_1, na.rm = T) == sum(wt$google, na.rm = T))
 })
 
 test_that("sum_visits errors", {
@@ -27,10 +30,10 @@ test_that("sum_visits errors", {
   wt <- as.wt_dt(testdt_tracking)
   wt <- extract_domain(wt)
   wt[, google := ifelse(domain == "google.com", 1, 0)]
-  wt[, search := ifelse(grepl("search", url), 1, 0)]
   expect_error(sum_visits(wt, timeframe = "not_a_variable"))
   wt[["wave"]] <- NULL
-  expect_error(sum_durations(wt, timeframe = "wave"))
+  expect_error(sum_visits(wt, timeframe = "wave"))
+  expect_error(sum_visits(wt, visit_class = "some_class_not_in_wt"))
 })
 
 test_that("sum_durations", {
@@ -42,7 +45,7 @@ test_that("sum_durations", {
   wt_sum <- sum_durations(wt)
   expect_true("duration_visits" %in% names(wt_sum))
   expect_true("panelist_id" %in% names(wt_sum))
-  expect_true(!"all" %in% names(wt_sum))
+  expect_true(length(names(wt_sum)) == 2)
   expect_true(length(unique(wt_sum$panelist_id)) == length(unique(wt$panelist_id)))
   expect_true(sum(wt$duration, na.rm = T) == sum(wt_sum$duration_visits, na.rm = T))
   wt_sum_date <- sum_durations(wt, timeframe = "date")
@@ -55,6 +58,10 @@ test_that("sum_durations", {
   expect_true("year" %in% names(wt_sum_year))
   wt_sum_wave <- sum_durations(wt, timeframe = "wave")
   expect_true("wave" %in% names(wt_sum_wave))
+  wt_sum_google <- sum_durations(wt, visit_class = "google")
+  expect_true("duration_visits_google_0" %in% names(wt_sum_google))
+  expect_true("duration_visits_google_1" %in% names(wt_sum_google))
+  expect_true(sum(wt_sum_google$duration_visits_google_1, na.rm = T) == sum(wt$duration[wt$google == 1], na.rm = T))
 })
 test_that("sum_durations errors", {
   data("testdt_tracking")
@@ -66,6 +73,7 @@ test_that("sum_durations errors", {
   expect_error(sum_durations(wt, timeframe = "not_a_variable"))
   wt[["wave"]] <- NULL
   expect_error(sum_durations(wt, timeframe = "wave"))
+  expect_error(sum_durations(wt, visit_class = "some_class_not_in_wt"))
 })
 test_that("sum_activity", {
   data("testdt_tracking")
