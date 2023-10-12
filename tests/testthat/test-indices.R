1c19ee4c81dfc7adb36328f022407387e80fcfa8
schochastics <david@schochastics.net>
2023-10-12 14:17:00 +0200

    added tests

--- tests/testthat/test-indices.R
+++ tests/testthat/test-indices.R
@@ -1,14 +1,45 @@
 test_that("isolation index works", {
-  left <-  c(5,5,0,0)
-  right <- c(0,0,5,5)
-  expect_equal(isolation_index(left,right),1)
-  left <-  c(5,5,5,5)
-  right <- c(5,5,5,5)
-  expect_equal(isolation_index(left,right),0)
+    left <- c(5, 5, 0, 0)
+    right <- c(0, 0, 5, 5)
+    expect_equal(isolation_index(left, right), 1)
+    left <- c(5, 5, 5, 5)
+    right <- c(5, 5, 5, 5)
+    expect_equal(isolation_index(left, right), 0)
 })
 
 test_that("isolation index errors", {
-  left <-  c(5,5,0)
-  right <- c(0,0,5,5)
-  expect_error(isolation_index(left,right))
+    left <- c(5, 5, 0)
+    right <- c(0, 0, 5, 5)
+    expect_error(isolation_index(left, right))
+})
+
+
+test_that("dissimilarity index works", {
+    left <- c(5, 5, 0, 0)
+    right <- c(0, 0, 5, 5)
+    expect_equal(dissimilarity_index(left, right), 1)
+    left <- c(5, 5, 5, 5)
+    right <- c(5, 5, 5, 5)
+    expect_equal(isolation_index(left, right), 0)
+})
+
+test_that("dissimilarity index errors", {
+    left <- c(5, 5, 0)
+    right <- c(0, 0, 5, 5)
+    expect_error(dissimilarity_index(left, right))
+})
+
+test_that("atkinson index works", {
+    left <- c(5, 5, 0, 0)
+    right <- c(0, 0, 5, 5)
+    expect_equal(atkinson_index(left, right), 1)
+    left <- c(5, 5, 5, 5)
+    right <- c(5, 5, 5, 5)
+    expect_equal(atkinson_index(left, right), 0)
+})
+
+test_that("atkinson index errors", {
+    left <- c(5, 5, 0)
+    right <- c(0, 0, 5, 5)
+    expect_error(atkinson_index(left, right))
 })
